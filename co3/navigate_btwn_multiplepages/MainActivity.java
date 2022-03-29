74a62239c2ea177575da7d623662f55f932b6e56
fathima shahana i.p <79891497+Fathimashahanaip@users.noreply.github.com>
2022-03-29 12:26:01 +0530

    Add files via upload
    sew

---
+++ co3/navigate_btwn_multiplepages/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.exam_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.bf);
+        Intent intent=new Intent(this,MainActivity2.class);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ co3/navigate_btwn_multiplepages/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.exam_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button b2=(Button) findViewById(R.id.bs);
+        Intent intent=new Intent(this,MainActivity.class);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}
